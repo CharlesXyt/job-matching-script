94d5e11ad40548abdc29f1454b80849d70a40321
Yitong Xiao <yitongxiao@Yitongs-MacBook-Pro.local>
2024-02-02 10:54:51 +1100

    restructure code and tests

--- run.py
+++ run.py
@@ -8,95 +8,102 @@ from typing import List
 import sys
 
 
-def read_csv_in_chunk(path:str):
-    return pd.read_csv(path, chunksize=PANDAS_CHUNK_SIZE)
+class DataLoader:
+    def __init__(self, db_url):
+        self.db = SQLiteDB(db_url)
+
+    @staticmethod
+    def read_csv_in_chunk(path:str):
+        return pd.read_csv(path, chunksize=PANDAS_CHUNK_SIZE)
 
 
-def update_skills(session, unique_skills: List):
-    existed_skills = session.query(SkillModel).filter(SkillModel.name.in_(unique_skills)).all()
+    def update_skills(self, unique_skills: List):
+        with self.db.get_session() as session:
+            existed_skills = session.query(SkillModel).filter(SkillModel.name.in_(unique_skills)).all()
 
-    existed_skills_set = set(s.name for s in existed_skills)
-    new_unique_skills_models = [SkillModel(name=skill_name) 
-                                for skill_name in unique_skills if skill_name not in existed_skills_set]
+            existed_skills_set = set(s.name for s in existed_skills)
+            new_unique_skills_models = [SkillModel(name=skill_name) 
+                                        for skill_name in unique_skills if skill_name not in existed_skills_set]
+            
+            session.bulk_save_objects(new_unique_skills_models)
+            session.commit()
+
+
+
+    def load_file_into_db(self, job_file_path:str, seeker_file_path:str):
+        jobs_df = self.read_csv_in_chunk(job_file_path)
+        seekers_df = self.read_csv_in_chunk(seeker_file_path)
     
-    session.bulk_save_objects(new_unique_skills_models)
-    session.commit()
+        for chunk in jobs_df:
+            chunk[['id', 'title']].to_sql(JobModel.__tablename__, self.db.engine, if_exists='replace', index=False)
+            unique_skills = set(chunk['required_skills'].str.split(',').explode().str.strip())
+            self.update_skills(unique_skills)
 
+            with self.db.get_session() as session:
+                for _, row in chunk.iterrows():
+                    job_id = row['id']
+                    skills = set(s.strip() for s in row['required_skills'].split(','))
+                    job_obj = session.query(JobModel).filter(JobModel.id==int(job_id)).first()
+                    for skill in skills:
+                        skill_obj = session.query(SkillModel).filter(SkillModel.name==skill).first()
+                        job_obj.skills.append(skill_obj)
 
+                session.commit()
 
-def load_file_into_db(db, job_file_path:str, seeker_file_path:str):
-    jobs_df = read_csv_in_chunk(job_file_path)
-    seekers_df = read_csv_in_chunk(seeker_file_path)
-   
-    for chunk in jobs_df:
-        chunk[['id', 'title']].to_sql(JobModel.__tablename__, db.engine, if_exists='replace', index=False)
-        unique_skills = set(chunk['required_skills'].str.split(',').explode().str.strip())
-        with db.get_session() as session:
-            update_skills(session, unique_skills)
-            for _, row in chunk.iterrows():
-                job_id = row['id']
-                skills = set(s.strip() for s in row['required_skills'].split(','))
-                job_obj = session.query(JobModel).filter(JobModel.id==int(job_id)).first()
-                for skill in skills:
-                    skill_obj = session.query(SkillModel).filter(SkillModel.name==skill).first()
-                    job_obj.skills.append(skill_obj)
+        for chunk in seekers_df:
+            chunk[['id', 'name']].to_sql(JobSeekerModel.__tablename__, self.db.engine, if_exists='replace', index=False)
+            unique_skills = set(chunk['skills'].str.split(',').explode().str.strip())
+            self.update_skills(unique_skills)
+            with self.db.get_session() as session:
+                for _, row in chunk.iterrows():
+                    seeker_id = row['id']
+                    skills = set(s.strip() for s in row['skills'].split(','))
+                    seeker_obj = session.query(JobSeekerModel).filter(JobSeekerModel.id==int(seeker_id)).first()
+                    for skill in skills:
+                        skill_obj = session.query(SkillModel).filter(SkillModel.name==skill).first()
+                        seeker_obj.skills.append(skill_obj)
+                session.commit()
 
-            session.commit()
+    def generate_result(self, use_sql=True):
+        if use_sql:
+            sql = '''
+                select
+                    seekers."id" as jobseeker_id, 
+                    seekers."name" as jobseeker_name,
+                    jobs."id" as job_id, 
+                    jobs."title" as job_title, 
+                    count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) as matching_skill_count, 
+                    round(count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) * 100 / count(DISTINCT job_skill."skill_id"), 2)  as matching_skill_percent
+                from jobs join job_skill on jobs."id" = job_skill."job_id"
+                cross join seekers join seeker_skill on seekers."id" = seeker_skill."seeker_id"
+                group by seekers."id", seekers."name", jobs."title"
+                having count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) > 0
+                order by 1 asc, 6 desc, 3 asc
+            '''
+            df = pd.read_sql_query(sql, self.db.engine)
+        else:
+            result = []
+            with self.db.get_session() as session:
+                seekers = session.query(JobSeekerModel).all()
+                jobs = session.query(JobModel).all()
+                for seeker in seekers:
+                    for job in jobs:
+                        seeker_skills = set(s.id for s in seeker.skills)
+                        job_skills = set(s.id for s in job.skills)
+                        match_skills = seeker_skills.intersection(job_skills)
+                        if len(match_skills) > 0:
+                            result.append({
+                                "jobseeker_id": seeker.id, 
+                                "jobseeker_name": seeker.name, 
+                                "job_id": job.id, 
+                                "job_title": job.title, 
+                                "matching_skill_count": len(match_skills),
+                                "matching_skill_percent":round(len(match_skills) * 100 / len(job_skills), 2)
+                            })
+            sorted_result = sorted(result, key=lambda x: [x["jobseeker_id"], -x["matching_skill_percent"], x["job_id"]])
+            df = pd.DataFrame(sorted_result)
 
-    for chunk in seekers_df:
-        chunk[['id', 'name']].to_sql(JobSeekerModel.__tablename__, db.engine, if_exists='replace', index=False)
-        unique_skills = set(chunk['skills'].str.split(',').explode().str.strip())
-        with db.get_session() as session:
-            update_skills(session, unique_skills)
-            for _, row in chunk.iterrows():
-                seeker_id = row['id']
-                skills = set(s.strip() for s in row['skills'].split(','))
-                seeker_obj = session.query(JobSeekerModel).filter(JobSeekerModel.id==int(seeker_id)).first()
-                for skill in skills:
-                    skill_obj = session.query(SkillModel).filter(SkillModel.name==skill).first()
-                    seeker_obj.skills.append(skill_obj)
-            session.commit()
-
-def generate_result(db, use_sql=True):
-    if use_sql:
-        sql = '''
-            select
-                seekers."id" as jobseeker_id, 
-                seekers."name" as jobseeker_name,
-                jobs."id" as job_id, 
-                jobs."title" as job_title, 
-                count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) as matching_skill_count, 
-                round(count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) * 100 / count(DISTINCT job_skill."skill_id"), 2)  as matching_skill_percent
-            from jobs join job_skill on jobs."id" = job_skill."job_id"
-            cross join seekers join seeker_skill on seekers."id" = seeker_skill."seeker_id"
-            group by seekers."id", seekers."name", jobs."title"
-            having count(CASE WHEN job_skill."skill_id" = seeker_skill."skill_id" THEN 1 END) > 0
-            order by 1 asc, 6 desc, 3 asc
-        '''
-        df = pd.read_sql_query(sql, db.engine)
-    else:
-        result = []
-        with db.get_session() as session:
-            seekers = session.query(JobSeekerModel).all()
-            jobs = session.query(JobModel).all()
-            for seeker in seekers:
-                for job in jobs:
-                    seeker_skills = set(s.id for s in seeker.skills)
-                    job_skills = set(s.id for s in job.skills)
-                    match_skills = seeker_skills.intersection(job_skills)
-                    if len(match_skills) > 0:
-                        result.append({
-                            "jobseeker_id": seeker.id, 
-                            "jobseeker_name": seeker.name, 
-                            "job_id": job.id, 
-                            "job_title": job.title, 
-                            "matching_skill_count": len(match_skills),
-                            "matching_skill_percent":round(len(match_skills) * 100 / len(job_skills), 2)
-                        })
-        sorted_result = sorted(result, key=lambda x: [x["jobseeker_id"], -x["matching_skill_percent"], x["job_id"]])
-        df = pd.DataFrame(sorted_result)
-
-    return df
+        return df
 
     
 
@@ -125,9 +132,9 @@ def main():
             print("Error: Please provide two file paths as command-line arguments.")
             print("Usage: python run.py generate <path_to_jobs.csv> <path_to_jobseekers.csv>")
             sys.exit(1)
-        db = SQLiteDB(DATABASE_URL)
-        load_file_into_db(db, sys.argv[2], sys.argv[3])
-        df = generate_result(db)
+        dl = DataLoader(DATABASE_URL)
+        dl.load_file_into_db(sys.argv[2], sys.argv[3])
+        df = dl.generate_result()
         print(df.to_string(index=False))
 
 
--- run_test.py
+++ run_test.py
@@ -5,10 +5,7 @@ from db import SQLiteDB
 from models.models import Base, SkillModel, JobModel, JobSeekerModel
 import pytest
 from run import (
-    read_csv_in_chunk,
-    update_skills,
-    load_file_into_db,
-    generate_result,
+    DataLoader
 )
 
 job_test_data = [
@@ -45,34 +42,36 @@ def create_dummy_csv():
     os.remove(seeker_file_path)
 
 @pytest.fixture(scope="function")
-def in_memory_db():
-    db = SQLiteDB('sqlite:///:memory:')
-    Base.metadata.create_all(db.engine)
+def in_memory_data_loader():
+    data_loader = DataLoader('sqlite:///:memory:')
+    
+    Base.metadata.create_all(data_loader.db.engine)
 
-    yield db
+    yield data_loader
 
-    Base.metadata.drop_all(db.engine)
+    Base.metadata.drop_all(data_loader.db.engine)
 
 @pytest.mark.usefixtures("create_dummy_csv")
 def test_read_csv_in_chunk(create_dummy_csv):
     job_file, _ = create_dummy_csv
-    chunks = read_csv_in_chunk(job_file)
+    
+    chunks = DataLoader.read_csv_in_chunk(job_file)
     assert isinstance(chunks, pd.io.parsers.TextFileReader)
 
-@pytest.mark.usefixtures("in_memory_db")
-def test_update_skills(in_memory_db):
+@pytest.mark.usefixtures("in_memory_data_loader")
+def test_update_skills(in_memory_data_loader):
     unique_skills = ['Python', 'SQL', 'Team Work']
-    with in_memory_db.get_session() as session:
+    with in_memory_data_loader.db.get_session() as session:
         session.add(SkillModel(name='Python'))
         session.commit()
-        update_skills(session, unique_skills)
+        in_memory_data_loader.update_skills(unique_skills)
         assert len(unique_skills) == len(session.query(SkillModel).all())
 
-@pytest.mark.usefixtures("in_memory_db", "create_dummy_csv")
-def test_load_file_into_db(in_memory_db, create_dummy_csv):
+@pytest.mark.usefixtures("in_memory_data_loader", "create_dummy_csv")
+def test_load_file_into_db(in_memory_data_loader, create_dummy_csv):
     job_file, job_seeker_file = create_dummy_csv
-    load_file_into_db(in_memory_db, job_file, job_seeker_file)
-    with in_memory_db.get_session() as session:
+    in_memory_data_loader.load_file_into_db(job_file, job_seeker_file)
+    with in_memory_data_loader.db.get_session() as session:
         jobs = session.query(JobModel).all()
         seekers = session.query(JobSeekerModel).all()
         assert len(jobs) == len(job_test_data) - 1
@@ -84,22 +83,22 @@ def test_load_file_into_db(in_memory_db, create_dummy_csv):
         assert set([s.name for s in ruby_developer_job.skills]) == set(['Ruby', 'SQL', 'Problem Solving'])
 
 
-@pytest.mark.usefixtures("in_memory_db", "create_dummy_csv")
-def test_generate_result_sql(in_memory_db, create_dummy_csv):
+@pytest.mark.usefixtures("in_memory_data_loader", "create_dummy_csv")
+def test_generate_result_sql(in_memory_data_loader, create_dummy_csv):
     job_file, job_seeker_file = create_dummy_csv
-    load_file_into_db(in_memory_db, job_file, job_seeker_file)
-    df = generate_result(in_memory_db, True)
+    in_memory_data_loader.load_file_into_db(job_file, job_seeker_file)
+    df = in_memory_data_loader.generate_result(True)
     alice_ruby_row = df[(df['jobseeker_name'] == 'Alice Seeker') & (df['job_title'] == 'Ruby Developer')]
     alice_backend_row = df[(df['jobseeker_name'] == 'Alice Seeker') & (df['job_title'] == 'Backend Developer')]
     assert alice_ruby_row['matching_skill_percent'].values[0] == round(3 * 100 / 3, 2)
     assert alice_backend_row['matching_skill_percent'].values[0] == round(2 * 100 / 4, 2)
 
 
-@pytest.mark.usefixtures("in_memory_db", "create_dummy_csv")
-def test_generate_result_no_sql(in_memory_db, create_dummy_csv):
+@pytest.mark.usefixtures("in_memory_data_loader", "create_dummy_csv")
+def test_generate_result_no_sql(in_memory_data_loader, create_dummy_csv):
     job_file, job_seeker_file = create_dummy_csv
-    load_file_into_db(in_memory_db, job_file, job_seeker_file)
-    df = generate_result(in_memory_db, False)
+    in_memory_data_loader.load_file_into_db(job_file, job_seeker_file)
+    df = in_memory_data_loader.generate_result(False)
     alice_ruby_row = df[(df['jobseeker_name'] == 'Alice Seeker') & (df['job_title'] == 'Ruby Developer')]
     alice_backend_row = df[(df['jobseeker_name'] == 'Alice Seeker') & (df['job_title'] == 'Backend Developer')]
     assert alice_ruby_row['matching_skill_percent'].values[0] == round(3 * 100 / 3, 2)
